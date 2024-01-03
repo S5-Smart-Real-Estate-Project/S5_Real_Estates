837fb3ba1c3f061d920286162e6c316f4a00fadc
sameer302 <sameernilkhan11@gmail.com>
2024-01-03 08:34:33 +0530

    Completed profile page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,7 @@ export const google = async (req, res, next) => {
           Math.random().toString(36).slice(-8) +
           Math.random().toString(36).slice(-8);
         const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-        const newUser = new User({username:req.body.name.split(' ').join('').toLowerCase() + Math.random().toString(36).slice(-4),
+        const newUser = new User({username:req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4),
           email: req.body.email,
           password: hashedPassword,
           avatar: req.body.photo,
