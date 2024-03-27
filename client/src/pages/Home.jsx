cf4d5e0f27700e3ae26755fdcd68b5e1eaaf5a01
sameer302 <sameernilkhan11@gmail.com>
2024-03-27 16:57:49 +0530

    Updated Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -50,12 +50,12 @@ export default function Home() {
     {/* top */}
     <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
       <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
-        Find your next <span className='text-slate-500'>perfect</span>
+        S5 Real Estates <span className='text-slate-500'>makes it easy</span> to find
         <br />
-        place with ease
+        your dream property
       </h1>
       <div className='text-gray-400 text-xs sm:text-sm'>
-        S5 Real Estate makes it easy to find
+        S5 Real Estate is the best place to find your next perfect place to live
         your dream property
         <br />
         We have a wide range of properties for you to choose from.
